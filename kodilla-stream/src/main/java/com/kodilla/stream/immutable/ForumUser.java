b04c0795cca15937ca9aab5f503fd255a6f216ad
ankleszczewski <an.kleszczewski@gmail.com>
2017-09-06 20:08:31 +0200

    Stream Third Commit

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -4,16 +4,16 @@ public final class ForumUser {
     private final String username;
     private final String realName;
 
-    private ForumUser(final String username, final String realName) {
+    ForumUser(final String username, final String realName) {
         this.username = username;
         this.realName = realName;
     }
 
-    private String getUsername() {
+    String getUsername() {
         return username;
     }
 
-    private String getRealName() {
+    String getRealName() {
         return realName;
     }
 }
