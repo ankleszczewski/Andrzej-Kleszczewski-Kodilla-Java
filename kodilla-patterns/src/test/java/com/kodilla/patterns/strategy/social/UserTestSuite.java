3c40d1d945d1dc55f7a3e3be2383e1e46f2f3b1d
ankleszczewski <an.kleszczewski@gmail.com>
2017-10-06 22:07:58 +0200

    10.3 First Commit

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.strategy.social;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTestSuite {
+    @Test
+    public void testDefaultSharingStrategies() {
+        //Given
+        User richard = new Millenials("Richard Smith");
+        User john = new YGeneration("John Connor");
+        User kodilla = new ZGeneration("Kodilla");
+        //When
+        String richardShouldUse = richard.sharePost();
+        System.out.println("Richard should use: " + richardShouldUse);
+        String johnShouldUse = john.sharePost();
+        System.out.println("John should use: " + johnShouldUse);
+        String kodillsShouldUse = kodilla.sharePost();
+        System.out.println("Kodills should use: " + kodillsShouldUse);
+        //Then
+        Assert.assertEquals(richardShouldUse, "Snapchat");
+        Assert.assertEquals(johnShouldUse, "Facebook");
+        Assert.assertEquals(kodillsShouldUse, "Twitter");
+    }
+}
