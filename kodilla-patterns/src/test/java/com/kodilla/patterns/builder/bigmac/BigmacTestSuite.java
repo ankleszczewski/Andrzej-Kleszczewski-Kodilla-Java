b85d63af8981ed8e0404854f9e582e20d606ecae
ankleszczewski <an.kleszczewski@gmail.com>
2017-10-08 14:37:49 +0200

    11.5 First Commit

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredient("Cheese")
+                .roll("Sesame")
+                .burgers(2)
+                .sauce("1000 wysp")
+                .ingredient("Onion")
+                .ingredient("Cucumber")
+                .ingredient("Extra bacon")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(4, howManyIngredients);
+    }
+}
